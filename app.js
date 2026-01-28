09bab242d3a63d1ad0fb7fa54a75cc887e1a5b65
tanmay5268 <tanmay.mewati94@gmail.com>
2026-01-28 20:00:21 +0530

    first commit - " made project setup both client n server" --next: " implementing socket logic and handle players move"

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const socket = require('socket.io');
+const http = require('http');
+const { Chess } = require('chess.js');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+let players = {};
+let currentPlayer = 'W';
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Multiplayer Chess Game' });
+});
+io.on("connection", (socket) => {
+    console.log('New client connected: ' + socket.id);
+});
+server.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
